19254b1cd570852d4579e66f852ec725422a704f
MKPNJoanne <97240800+MKPNJoanne@users.noreply.github.com>
2025-08-05 13:54:42 +0530

    Initial Yii2 project for LayInvest

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'F5Hwgg7aMvpfJ_C-lFrGjc-9npRns3dG',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
